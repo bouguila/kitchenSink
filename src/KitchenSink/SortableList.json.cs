83363734d148625bf7954e1a48b13e4674b045fe
jawher@me.com <yawyaw12>
2017-03-02 00:59:52 +0100

    general fixes

--- src/KitchenSink/SortableList.json.cs
+++ src/KitchenSink/SortableList.json.cs
@@ -11,6 +11,20 @@ namespace KitchenSink
         public string FirstName;
         public string LastName;
         public int Rank;
+
+        public bool isLast {
+            get {
+                Int64 count = Db.SlowSQL<Int64>("select count(*) from Person").First;
+                return count==(Rank+1);
+            }
+        }
+        public bool isFirst
+        {
+            get
+            {
+                return Rank == 0;
+            }
+        }
     }
 
 
@@ -19,7 +33,6 @@ namespace KitchenSink
         protected override void OnData()
         {
             base.OnData();
-            Debug.WriteLine("***************************************************************************");
             Db.Transact(() =>
             {
                 var anyone = Db.SQL<Person>("SELECT p FROM Person p").First;
@@ -29,34 +42,74 @@ namespace KitchenSink
                     {
                         FirstName = "John",
                         LastName = "Doe",
-                        Rank = 1
+                        Rank = 0
                     };
                     new Person
                     {
                         FirstName = "test",
                         LastName = "user",
-                        Rank = 2
+                        Rank = 1
                     };
                     new Person
                     {
                         FirstName = "jawher",
                         LastName = "bouguila",
-                        Rank = 3
+                        Rank = 2
                     };
                     new Person
                     {
                         FirstName = "star",
                         LastName = "counter",
-                        Rank = 4
+                        Rank = 3
                     };
                 }
             });
 
-            var person = Db.SQL<Person>("SELECT p FROM Person p");
+            var person = Db.SQL<Person>("SELECT p FROM Person p ORDER BY p.Rank");
             Persons.Data = person;
         }
 
-        
+        void Handle(Input.Up action)
+        {
+      
+            
+
+            Debug.WriteLine(action);
+            if (this.Persons == null)
+            {
+                return;
+            }
+
+
+            var tmp = Persons[0];
+           
+
+            Db.Transact(() =>
+            {
+                Persons[0].Rank++;
+                Persons[1].Rank--;
+                Persons[0] = Persons[1];
+                Persons[1] = tmp;
+                
+            });
+
+            //var tmp = Persons[0];
+            //Persons[0] = Persons[1];
+            //Persons[1] = tmp;
+            //Persons[0].Rank++;
+            //Persons[1].Rank--;
+
+
+        }
+
+        void Handle(Input.Down action)
+        {
+            Debug.WriteLine("hello");
+            //get the index of person
+
+
+            //Transaction.Commit();
+        }
 
 
 
